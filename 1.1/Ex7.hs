ca696afdaf3c372b4a2657334839307e34fb217c
Rien Heuver <rienheuver@gmail.com>
2016-04-19 11:57:38 +0200

    Ex7 deel 1

--- 1.1/Ex7.hs
+++ 1.1/Ex7.hs
@@ -1,5 +1,2 @@
 r :: Num a => a -> a -> [a]
-r start dif = rrecur [start] start dif
-
-rrecur :: Num a => [a] -> a -> a -> [a]
-rrecur list last dif = rrecur (list ++ [last+dif]) (last+dif) dif
+r start dif = start : r (start+dif) dif
