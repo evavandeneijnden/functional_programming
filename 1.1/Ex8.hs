915e289dd4a0dc0b82601d091f362b655e68a931
eva <evavandeneijnden@gmail.com>
2016-04-20 11:29:07 +0200

    Exercise 8.2 of series 1.1

--- 1.1/Ex8.hs
+++ 1.1/Ex8.hs
@@ -4,3 +4,13 @@ allEqual (x:xs)     | length xs == 0 = True
                     | length xs >= 2 && x == head xs = allEqual xs
                     | length xs == 1 && x == head xs = True
                     | otherwise = False
+
+f :: Num a => a -> a -> a
+f element amount = element - amount
+
+
+isAS :: (Eq a, Num a) => [a] -> Bool
+isAS [] = False
+isAS [x] = False
+isAS [x,y] = True
+isAS (x:y:z:zs) = (x-y) == (y - z) && isAS (y:z:zs)
