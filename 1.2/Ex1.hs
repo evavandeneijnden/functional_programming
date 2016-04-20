05319ec7f2d2a53f48386f718f5b41ec9e2c539d
Rien Heuver <rienheuver@gmail.com>
2016-04-20 14:16:31 +0200

    Implemented myZipWith

--- 1.2/Ex1.hs
+++ 1.2/Ex1.hs
@@ -3,3 +3,7 @@ myfilter func (x:xs)    | length xs == 0 && func x == True = [x]
                         | length xs == 0 && func x == False = []
                         | func x == True = x : myfilter func xs
                         | func x == False = myfilter func xs
+
+myZipWith :: (a -> b -> c) -> [a] -> [b] -> [c]
+myZipWith func (x:xs) (y:ys)                    | length xs == 0 || length ys == 0 = [func x y]
+                                                | otherwise = func x y : myZipWith func xs ys
