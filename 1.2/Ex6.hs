3597d92bee3d223fc2225e1bd3f3bc9459c250b3
Rien Heuver <rienheuver@gmail.com>
2016-04-21 14:40:12 +0200

    Ex6

--- 1.2/Ex6.hs
+++ 1.2/Ex6.hs
@@ -1,3 +1,19 @@
-sublist :: [a] -> [a] -> [a]
+sublist :: Eq a => [a] -> [a] -> Bool
 
-sublist xs ys =
+sublist [] [] = True
+sublist xs [] = True
+sublist [] ys = False
+sublist (x:xs) (y:ys)   | x /= y = sublist xs (y:ys)
+                        | x == y = sublistHelper xs ys
+sublistHelper [] [] = True
+sublistHelper xs [] = True
+sublistHelper [] ys = False
+sublistHelper (x:xs) (y:ys)                     | x /= y = False
+                                                | x == y = sublistHelper xs ys
+
+partSublist :: Eq a => [a] -> [a] -> Bool
+partSublist [] [] = True
+partSublist xs [] = True
+partSublist [] ys = False
+partSublist (x:xs) (y:ys)                       | x /= y = partSublist xs (y:ys)
+                                                | x == y = partSublist xs ys
