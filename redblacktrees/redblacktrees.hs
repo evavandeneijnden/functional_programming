93b52fc332ac8229ac033b17019224334c3dd01e
Rien Heuver <rienheuver@gmail.com>
2016-06-19 19:02:34 +0200

    Insertion fixed

--- redblacktrees/redblacktrees.hs
+++ redblacktrees/redblacktrees.hs
@@ -1,3 +1,4 @@
+import Debug.Trace
 import FPPrac.Trees
 import RBT
 
@@ -7,12 +8,28 @@ insert (Node color value leftTree rightTree) new_value  | new_value < value = No
 insert (Leaf colour) new_value = Node Red new_value (Leaf Black) (Leaf Black)
 
 fixRed :: RBT -> RBT
--- case 1
-fixRed (Node Red x (Node Red y yt1 yt2) t2) = Node Black x (Node Red y yt1 yt2) t2
-fixRed (Node Red x t1 (Node Red y yt1 yt2)) = Node Black x t1 (Node Red y yt1 yt2)
+-- Goes down the tree to start upwards-fixing
+fixRed (Node colour x t1 t2) = fixRedRoot (fixRed' newTree)
+                        where
+                            newTree = (Node colour x (fixRedChild t1) (fixRedChild t2))
+fixRed (Leaf colour) = Leaf colour
 
+fixRedChild :: RBT -> RBT
+fixRedChild (Node colour x t1 t2) = fixRed' newTree
+                        where
+                            newTree = (Node colour x (fixRedChild t1) (fixRedChild t2))
+fixRedChild (Leaf colour) = Leaf colour
+
+fixRedRoot :: RBT -> RBT
+-- case 1
+fixRedRoot (Node Red x t1 t2) = Node Black x t1 t2
+-- Don't fix what ain't broken aye?
+fixRedRoot tree = tree
+
+-- Fixes upwards
+fixRed' :: RBT -> RBT
 -- case 2
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 (Node Red gc1 ggc1 ggc2)
                 gc2
@@ -21,8 +38,8 @@ fixRed (Node Black b
                 gc3
                 gc4
             )
-        ) = fixRed (
-        Node Red b
+        ) =
+        (Node Red b
             (Node Black r1
                 (Node Red gc1 ggc1 ggc2)
                 gc2
@@ -32,7 +49,7 @@ fixRed (Node Black b
                 gc4
             )
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 gc1
                 (Node Red gc2 ggc1 ggc2)
@@ -41,8 +58,8 @@ fixRed (Node Black b
                 gc3
                 gc4
             )
-        ) = fixRed (
-        Node Red b
+        ) =
+        (Node Red b
             (Node Black r1
                 gc1
                 (Node Red gc2 ggc1 ggc2)
@@ -52,7 +69,7 @@ fixRed (Node Black b
                 gc4
             )
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 gc1
                 gc2
@@ -61,8 +78,8 @@ fixRed (Node Black b
                 (Node Red gc3 ggc1 ggc2)
                 gc4
             )
-        ) = fixRed (
-        Node Red b
+        ) =
+        (Node Red b
             (Node Black r1
                 gc1
                 gc2
@@ -72,7 +89,7 @@ fixRed (Node Black b
                 gc4
             )
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 gc1
                 gc2
@@ -81,8 +98,8 @@ fixRed (Node Black b
                 gc3
                 (Node Red gc4 ggc1 ggc2)
             )
-        ) = fixRed (
-        Node Red b
+        ) =
+        (Node Red b
             (Node Black r1
                 gc1
                 gc2
@@ -93,7 +110,7 @@ fixRed (Node Black b
             )
         )
 -- case 3
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 (Node Red gc1 ggc1 ggc2)
                 gc2
@@ -103,9 +120,8 @@ fixRed (Node Black b
         (Node Black r1
             (Node Red gc1 ggc1 ggc2)
             (Node Red b gc2 c2)
-
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             (Node Red r1
                 gc1
                 (Node Red gc2 ggc1 ggc2)
@@ -116,7 +132,7 @@ fixRed (Node Black b
             (Node Red r1 gc1 ggc1)
             (Node Red b ggc2 c2)
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             c1
             (Node Red r1
                 (Node Red gc1 ggc1 ggc2)
@@ -126,29 +142,21 @@ fixRed (Node Black b
         (Node Black gc1
             (Node Red b c1 ggc1)
             (Node Red r1 ggc2 gc2)
-
         )
-fixRed (Node Black b
+fixRed' (Node Black b
             c1
             (Node Red r1
                 gc1
                 (Node Red gc2 ggc1 ggc2)
             )
-        ) =
+        ) = trace "Applying case 3.4"
         (Node Black r1
             (Node Red b c1 gc1)
             (Node Red gc2 ggc1 ggc2)
-
         )
 
--- Fixes upwards because it fixes itself and is matched in the root if the root is not matched.
--- If the root IS matched, it will fix itself and does not need to fix upwards.
-fixRed (Node colour x t1 t2) | (fixed newTree) = newTree
-                             | otherwise = fixRed (newTree)
-                        where
-                            newTree = (Node colour x (fixRed t1) (fixRed t2))
-
-fixRed (Leaf colour) = Leaf colour
+-- Don't fix what ain't broken aye?
+fixRed' tree = tree
 
 fixed :: RBT -> Bool
 fixed (Leaf c)  | c == Black = True
@@ -164,6 +172,10 @@ fixed' (Node Red _
             (Node Black _ grandchild1 grandchild2)
             (Node Black _ grandchild3 grandchild4)
         ) = (fixed' grandchild1) && (fixed' grandchild2) && (fixed' grandchild3) && (fixed' grandchild4)
+fixed' (Node Red _
+            (Leaf Black)
+            (Leaf Black)
+        ) = True
 fixed' _ = False
 
 
@@ -175,5 +187,5 @@ balancedInsert tree value = fixRed (insert tree value)
 testRBT = (Node Black 6 (Node Red 5 (Leaf Black) (Leaf Black)) (Node Red 7 (Leaf Black) (Leaf Black)))
 
 test = showRBTree   (showRBT
-                        (balancedInsert testRBT 9)
+                        (balancedInsert (balancedInsert (balancedInsert testRBT 9) 36) 8)
                     )
